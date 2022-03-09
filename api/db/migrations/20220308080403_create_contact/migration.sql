298b87e5b36cb892b36b812f1b6486157c7a325f
AGD <aloeverdulay@gmail.com>
2022-03-09 12:59:14 +0800

    Updated db migration and root README

--- api/db/migrations/20220308080403_create_contact/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
