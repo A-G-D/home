e6068a8075762c6fb8c6fc0608724a003a7002a5
AGD <aloeverdulay@gmail.com>
2022-03-22 13:16:16 +0800

    Updated storybook configuration - added viewport sizes

---
+++ web/config/storybook.preview.js
@@ -0,0 +1,27 @@
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+const viewports = {
+  phone: {
+    name: 'phone',
+    styles: {
+      width: '1080px',
+      height: '2246px',
+    },
+  },
+  tablet: {
+    name: 'tablet',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    },
+  },
+}
+
+export const parameters = {
+  viewports: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...viewports,
+    },
+  },
+}
