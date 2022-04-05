062db8f3a706c8b1bada3d4319ccdb59ca84c152
AGD <aloeverdulay@gmail.com>
2022-04-05 21:46:16 +0800

    Added global constants

---
+++ web/src/constants/constants.ts
@@ -0,0 +1,16 @@
+export const SiteStatus = {
+  UP: 0,
+  DOWN: 1,
+  UPGRADE: 2,
+  MAINTENANCE: 3,
+  CONSTRUCTION: 4,
+}
+
+export const Author = {
+  LOGO: 'https://a_g_d.keybase.pub/Pictures/logo.png',
+  FIRST_NAME: 'Aloever',
+  LAST_NAME: 'Dulay',
+  FULL_NAME: 'Aloever G. Dulay',
+  PSEUDONYM: 'AGD',
+  INITIALS: 'A-G-D',
+}
---
+++ web/src/constants/links.ts
@@ -0,0 +1,11 @@
+export const email = {
+  gmail: 'aloeverdulay@gmail.com',
+}
+
+export const profile = {
+  github: 'https://github.com/A-G-D',
+  linkedin: 'https://linkedin.com/in/aloever-dulay-249226159',
+  keybase: 'https://keybase.io/a_g_d',
+  keybase_pub: 'https://keybase.pub/a_g_d',
+  twitter: 'https://www.twitter.com/__A_G_D__',
+}
