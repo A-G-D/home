88c0a7b16d157ad5e964438e6f95809ed3dff400
AGD <aloeverdulay@gmail.com>
2022-04-10 23:02:57 +0800

    Added useResizeObserver hook

--- web/src/lib/hooks.ts
+++ web/src/lib/hooks.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@redwoodjs/web'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, MutableRefObject } from 'react'
 
 export const useSettings = () => {
   const QUERY = gql`
@@ -21,4 +21,18 @@ export const useRerender = () => {
   return () => {
     setValue((v) => v + 1)
   }
+}
+
+export const useResizeObserver = <T extends Element>(
+  onResizeHandler: ResizeObserverCallback,
+  deps: React.DependencyList = []
+) => {
+  const ref = useRef<T>()
+  useEffect(() => {
+    const innerElement = ref.current as Element
+    const resizeObserver = new ResizeObserver(onResizeHandler)
+    resizeObserver.observe(innerElement)
+    return () => resizeObserver.unobserve(innerElement)
+  }, deps)
+  return ref
 }
