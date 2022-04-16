f57ccbe44c483d7f804abff895d2c7e4bd6a45e1
AGD <aloeverdulay@gmail.com>
2022-04-16 23:45:41 +0800

    Added static assets to repository

---
+++ web/src/pages/HomePage/assets/assets.js
@@ -0,0 +1,50 @@
+import Html5SVG from './html5-original.svg'
+import Css3SVG from './css3-original.svg'
+import JavascriptSVG from './javascript-original.svg'
+import TypescriptSVG from './typescript-original.svg'
+import GitSVG from './git-original.svg'
+import ReactJSSVG from './react-original.svg'
+import TailwindcssSVG from './tailwindcss-plain.svg'
+import SassSVG from './sass-original.svg'
+import LessSVG from './less-plain-wordmark.svg'
+import StorybookSVG from './storybook-original.svg'
+import JestSVG from './jest-plain.svg'
+import WebpackSVG from './webpack-original.svg'
+import GithubSVG from './github-original.svg'
+import PythonSVG from './python-original.svg'
+import JupyterSVG from './jupyter-original-wordmark.svg'
+import CplusplusSVG from './cplusplus-original.svg'
+import CsharpSVG from './csharp-original.svg'
+import LuaSVG from './lua-original.svg'
+import BashSVG from './bash-original.svg'
+
+export const Html5 = (props) => <Html5SVG {...props} />
+export const Css3 = (props) => <Css3SVG {...props} />
+export const Javascript = (props) => <JavascriptSVG {...props} />
+export const Typescript = (props) => <TypescriptSVG {...props} />
+export const Git = (props) => <GitSVG {...props} />
+export const ReactJS = (props) => <ReactJSSVG {...props} />
+export const Tailwindcss = (props) => <TailwindcssSVG {...props} />
+export const Sass = (props) => <SassSVG {...props} />
+export const Less = (props) => <LessSVG {...props} />
+export const Storybook = (props) => <StorybookSVG {...props} />
+export const Jest = (props) => <JestSVG {...props} />
+export const Webpack = (props) => <WebpackSVG {...props} />
+export const Github = (props) => <GithubSVG {...props} />
+export const Python = (props) => <PythonSVG {...props} />
+export const Jupyter = (props) => <JupyterSVG {...props} />
+export const Cplusplus = (props) => <CplusplusSVG {...props} />
+export const Csharp = (props) => <CsharpSVG {...props} />
+export const Lua = (props) => <LuaSVG {...props} />
+export const Bash = (props) => <BashSVG {...props} />
+
+import EtchASketch from './a-g-d.github.io_TOP-etch-a-sketch.png'
+import LandingPage from './a-g-d.github.io_TOP-landing-page.png'
+import RockPaperScissors from './a-g-d.github.io_TOP-rock-paper-scissors.png'
+
+export const Screenshot = {
+  EtchASketch,
+  LandingPage,
+  RockPaperScissors,
+}
+export { default as Redwood } from './redwood.png'
